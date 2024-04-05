77256a27b2a9b705dfc2b47c64a06a48c5d12adf
Kelsey <139846095+kelseywwwt@users.noreply.github.com>
2024-04-04 20:50:16 -1000

    Add desert ground + movement only when game starts

--- main.lua
+++ main.lua
@@ -7,15 +7,18 @@ function love.load()
 
     -- BG Stars
     bgStars = {}
-    for i=1,25 do 
+    for i=1,50 do 
         table.insert(bgStars,
             {
                 x = love.math.random(0, 550),
-                y = love.math.random(0, 700),
+                y = love.math.random(0, -850 * 6),
                 image = love.graphics.newImage("img/bgStar.png")
             }
         )
     end
+
+    -- BG Desert (start point)
+    desert = love.graphics.newImage("img/desert.png")
 
     player = Player()
 end
@@ -24,11 +27,16 @@ function love.update(dt)
     player:update(dt)
 end
 
-function love.draw()
+function love.draw()      
+    -- Draw player
     player:draw()
 
+    -- Background decorations --
     -- Draw BG Stars randomly
     for i, v in ipairs(bgStars) do 
         love.graphics.draw(v.image, v.x, v.y) 
     end
+    
+    -- Draw BG Desert ground for starting point
+    love.graphics.draw(desert, -1, 750)
 end
